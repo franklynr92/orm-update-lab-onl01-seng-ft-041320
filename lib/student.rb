29de9c5af8f6756a912ec16cfb830adcffbc20e7
Franklyn Rodriguez <franklynr92@gmail.com>
2020-05-15 23:12:00 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
+attr_accessor :name, :id, :grade
 
+def initialize(name, grade, id=nil)
+  @name = name
+  @grade = grade
+  @id = id
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
